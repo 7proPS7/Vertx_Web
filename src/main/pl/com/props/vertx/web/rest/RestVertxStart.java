31dc2fd2f42d4dd504ee42383776915b8f69a7a8
7proPS7 <7props7@gmail.com>
2017-04-02 16:23:09 +0200

    change jre 1.5 -> 1.8, Use Router and create httpServer

--- src/main/pl/com/props/vertx/web/rest/RestVertxStart.java
+++ src/main/pl/com/props/vertx/web/rest/RestVertxStart.java
@@ -1,15 +1,39 @@
 package main.pl.com.props.vertx.web.rest;
 
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Future;
+import io.vertx.core.http.HttpServerResponse;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.handler.StaticHandler;
 
 public class RestVertxStart extends AbstractVerticle {
 	
+	
 	@Override
-	public void start(io.vertx.core.Future<Void> startFuture) throws Exception {
-		// TODO Auto-generated method stub
-		super.start(startFuture);
-	}
+	public void start(Future<Void> future) throws Exception {
 
+	
+		
+		Router router = Router.router(vertx);
+
+		router.route("/").handler(routingContext -> {
+			HttpServerResponse response = routingContext.response();
+			response.putHeader("content-type", "text/html").end("<h2>Web vertx app</h2>");
+		});
+		
+
+		router.route("/assets/*").handler(StaticHandler.create("assets"));
+		
+
+		vertx.createHttpServer().requestHandler(router::accept).listen(config().getInteger("http.port", 8080),
+				result -> {
+					if (result.succeeded()) {
+						future.complete();
+					} else {
+						future.fail(result.cause());
+					}
+				});
+	}
 
 
 }
