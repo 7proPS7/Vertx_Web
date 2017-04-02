82f8502f6998a0502bb5e39a058be816e39acaf2
7proPS7 <7props7@gmail.com>
2017-04-02 16:03:49 +0200

    add dependencies and plugin to(pom) create fat jar file plus START class

---
+++ src/main/pl/com/props/vertx/web/rest/RestVertxStart.java
@@ -0,0 +1,15 @@
+package main.pl.com.props.vertx.web.rest;
+
+import io.vertx.core.AbstractVerticle;
+
+public class RestVertxStart extends AbstractVerticle {
+	
+	@Override
+	public void start(io.vertx.core.Future<Void> startFuture) throws Exception {
+		// TODO Auto-generated method stub
+		super.start(startFuture);
+	}
+
+
+
+}
